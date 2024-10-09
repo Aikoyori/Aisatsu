1cb3771f859266dfca3e5bd30718db8f480276d6
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2024-10-10 03:54:50 +0700

    inital run pls kill me it s 4am

---
+++ resources/skin.js
@@ -0,0 +1,20 @@
+/* JavaScript for the Example skin */
+// button with id wm-menu-button will toggle class open in id wm-sidebar
+
+window.Aisatsu = window.Aisatsu || {};
+
+// use checkbox as menu toggle
+cacophony = function() {
+	$('#mw-menu-toggle').on('click', function(e) {
+		e.preventDefault();
+		$('#mw-sidebar').toggleClass('open');
+		$('#mw-dimmer').toggleClass('open');
+	});
+	$('#mw-dimmer').on('click', function(e) {
+		e.preventDefault();
+		$('#mw-sidebar').toggleClass('open');
+		$('#mw-dimmer').toggleClass('open');
+	});
+}
+
+cacophony();
